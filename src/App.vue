fa540d8c554090c84eec73f737e6363479cc0c3a
zhuangsd <hbsnjzsd@163.com>
2017-05-07 23:46:26 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <data-tables data='data from server'></data-tables>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import DataTables from './components/DataTables'
+
+export default {
+  name: 'app',
+  components: {
+    Hello,DataTables
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/DataTables.vue
@@ -0,0 +1,43 @@
+<template>  
+  <div class="datatables">
+    <table>
+    <th>
+    <td v-for="col in columns">col.text</td>
+    </th>
+
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'datatables',
+  props:['data'],
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js <App13131>'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
